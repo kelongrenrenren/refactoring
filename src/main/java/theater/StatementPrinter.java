616530ccc3f93dcb329e2c745c6596c98a3fc51c
kelongrenrenren <liuyx1130@gmail.com>
2025-11-24 16:08:48 -0500

    Task 2.2

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -35,11 +35,7 @@ public class StatementPrinter {
 
 
             // add volume credits
-            volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-            // add extra credit for every five comedy attendees
-            if ("comedy".equals(play.getType())) {
-                volumeCredits += p.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
-            }
+            volumeCredits += getVolumeCredits(p);
 
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n",
@@ -78,6 +74,16 @@ public class StatementPrinter {
             default:
                 throw new RuntimeException(String.format("unknown type: %s", play.getType()));
         }
+        return result;
+    }
+
+    private int getVolumeCredits(Performance performance) {
+        int result = Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+
+        if ("comedy".equals(getPlay(performance).getType())) {
+            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+
         return result;
     }
 
